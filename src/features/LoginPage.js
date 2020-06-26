60605f2fd4bcf0d93cdda657825e91578da4dbcf
Hamza Farooq <hamzafarooq25@gmail.com>
2020-06-26 20:02:41 +0500

    add: login signup page

--- src/features/LoginPage.js
+++ src/features/LoginPage.js
@@ -1,8 +1,130 @@
 import React from 'react'
+import {Formik, Form, Field} from 'formik'
+import * as Yup from 'yup'
+import { makeStyles } from '@material-ui/core/styles'
+import { Button, Container } from '@material-ui/core'
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
+import ToggleButton from '@material-ui/lab/ToggleButton'
+import { TextField } from 'formik-material-ui'
+import { connect } from 'react-redux'
+// import { login, clearError } from './userSlice'
+// import ErrorSnackbar from '../../ui/ErrorSnackbar'
+import landingBG from './landingBG.jpg'
 
+const useStyles = makeStyles(theme=>({
+	root: {
+    position: 'absolute',
+    maxWidth: '32vw',
+    marginLeft: 0,
+    marginTop: 0,
+    height: '100%',
+    backgroundColor: "#DBDBDA40",
+    color: theme.palette.secondary.main
+	},
+	input: {
+    color: 'black',
+	}
+  }))
+  
 export default function LoginPage() {
-	
+	const classes = useStyles()
 	return (
-		<h1>Login Page.</h1>
-	)
+    <Container component="main" className={classes.root}>
+      <img style={{position: 'absolute', left: '30vw', width: '70vw', height: '100vh'}}
+      src={landingBG} alt="RationGhar"/>
+      {/* <img style={{
+        position: 'absolute',
+        left: '3vw',
+        width: '12vw',
+        height: '10vh',
+        paddingTop: 800,
+        paddingLeft: 20,
+        paddingRight:10,
+      }}
+      src={lumslogo} alt="lumslogo"/> */}
+      
+
+      {/* <img style={{
+        position: 'absolute',
+        left: '3vw',
+        width: '25vw',
+        height: '20vh',
+        paddingTop: 50,
+        // paddingLeft: -10,
+        // paddingRight:-10,
+        borderColor: "#F5FFFA",
+      }}
+      src={cmsLogo} alt="cmsLogo"/> */}
+      
+      <div style={{marginTop: '30vh', marginLeft: '3vw'}}>
+      <Formik
+        validateOnChange={false} validateOnBlur={true}
+        initialValues = {{
+            email: '',
+            password: '',
+        }}
+        validationSchema={Yup.object({
+            email: Yup.string()
+              .email('Invalid Email Address')
+              .required('Required'),
+            password: Yup.string()
+              .required('Required')
+        })}
+        onSubmit={ (values, { setSubmitting }) => {
+            // dispatch(login({email: values.email, password: values.password, userType: userType}))
+            // .then(() => {
+            //   setSubmitting(false)
+            // }) 
+          }
+        }
+        >
+          {({submitForm, isSubmitting})=>(
+            <Form>
+              <h1 style={{color: "black"}}>Login</h1>
+
+              <Field
+                style = {{backgroundColor: 'white'}}
+                component={TextField}
+                variant="filled"
+                margin="normal"
+                required
+                label="Email"
+                name="email"
+                InputProps={{
+                  className: classes.input,
+                }}
+              ></Field>
+              <br/>            
+              <Field
+                style = {{backgroundColor: 'white'}}
+                component={TextField}
+                variant="filled"
+                margin="normal"
+                required
+                label="Password"
+                name="password"
+                type="password"
+                InputProps={{
+                  className: classes.input,
+                }}
+              > 
+              </Field>
+              
+              <br/>    
+              <br/>
+              <Button size="large" onClick={submitForm} type="submit"
+              variant="contained" color="secondary" spacing= '10'
+              endIcon={<NavigateNextIcon/>}>
+                Login
+              </Button>
+            </Form>
+          )}
+        </Formik>
+        
+        </div>
+          
+      {/* <ErrorSnackbar stateError={error} clearError={clearError}/> */}
+    </Container>
+  )
 }
--- src/features/SignUpPage.js
+++ src/features/SignUpPage.js
@@ -1,10 +1,173 @@
 import React from 'react'
+import {Formik, Form, Field} from 'formik'
+import * as Yup from 'yup'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import {Container, LinearProgress,Typography, Box} from '@material-ui/core'
+import { TextField } from 'formik-material-ui'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 // Add 2 more fields (facebook and instagram links ke liye)
+const useStyles = makeStyles((theme) => ({
+	root: {
+    flexGrow: 1,
+	},
+	rationGharTitle: {
+    padding: theme.spacing(2),
+    marginTop: 10,
+    color: theme.palette.text.primary
+	},
+}))
 
 export default function SignUpPage() {
-	
-	return (
-		<h1>Sign Up Page</h1>
-	)
+	const classes = useStyles()
+  return (
+    <Container component="main" maxWidth="xs"> 
+      <Typography variant="h4" className={classes.rationGharTitle}>
+        <Box fontWeight={700} textAlign="center">
+          Sign up for Ration Ghar
+        </Box>
+      </Typography>
+      <Formik
+        validateOnChange={false} validateOnBlur={true}
+        initialValues = {{
+          name: '',
+          email:'',
+          cnic: '',
+          phoneNumber: '',
+          location:'',
+          password:'',
+          confirmPassword:'',
+        }}
+        validationSchema = {Yup.object({
+          name: Yup.string()
+            .required('Required'),
+          email: Yup.string()
+            .required('Required'),
+        
+          cnic: Yup.string()
+            .required('Required'),
+
+          phoneNumber: Yup.string()
+            .required('Required'),
+            
+          location: Yup.string()
+            .required('Required')
+            ,
+          password: Yup.string()
+            .required('Required')
+            .min(8,'Must be at least 8 characters')
+            .max(30,'Must be atmost 30 characters')
+            .matches('^[a-zA-Z0-9]+$', 'All passwords must be alphanumeric (no special symbols).'),
+          confirmPassword: Yup.string()
+          .when("password",{
+            is: val => (val && val.length > 0 ? true : false),
+            then: Yup.string().oneOf(
+              [Yup.ref("newPassword")],"Both passwords need to be the same."
+            )
+          })
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          // dispatch(changePassword({ name: values.name, cnic: values.cnic}))
+          // .then(() => {
+          //   setSubmitting(false)
+          // })
+          console.log("Submitted")  
+        }}
+      >
+        {({onSubmit, isSubmitting})=>{
+          return(
+            <Form>
+              <p>Enter the Information Required</p>
+
+              <Field
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                required
+                label="Name"
+                name="name"
+                fullWidth
+              ></Field>
+              <br/>
+
+              <Field
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Email address"
+                name="email"
+              ></Field>
+
+
+              <Field
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="CNIC"
+                name="cnic"
+              ></Field>
+
+              <br/>
+
+              <Field
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Phone Number"
+                name="Phone Number"
+              ></Field>
+              <br/>
+
+              <Field
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Location"
+                name="location"
+              ></Field>
+              <br/>
+              
+              <Field
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Password"
+                name="password"
+              ></Field>
+              <br/>
+
+              <Field
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Confirm Password"
+                name="confirmPassword"
+              ></Field>
+              <br/>
+
+              {isSubmitting && <LinearProgress />}
+              <Button type="submit" variant="contained" color="primary" spacing= '10' onClick={onSubmit} >Submit Form</Button>
+              {/* <Button variant="contained" spacing= '10' onClick={() => history.goBack()} style={{marginLeft: 30}}>Back</Button> */}
+            
+            </Form>
+          )
+        }}
+      </Formik>
+    </Container>
+    )
+
 }
